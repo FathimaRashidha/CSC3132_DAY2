61d1a36e551f1a5bc5ebd7db1a4cb67a1b2b6c4a
FathimaRashidha <120313657+FathimaRashidha@users.noreply.github.com>
2024-12-01 17:15:30 +0000

    change the code again

--- public/index1.php
+++ public/index1.php
@@ -1,8 +1,8 @@
 <?php
-
+//get the db connection file
 require_once 'dbconf.php';
 
-function PrintTable(tableName,$connect)
+function PrintTable($tableName,$connect)
 {
 
 try {
@@ -45,4 +45,100 @@ try {
 }
 
 
-PrintTable("student",$connect);
+//PrintTable("books",$connect);
+<?php
+//get the db connection file
+require_once 'dbconf.php';
+
+function PrintTable($tableName,$connect)
+{
+
+try {
+
+	//Query
+	$sql = "SELECT * FROM $tableName";
+
+	//excute the quey
+	$result = mysqli_query($connect,$sql);
+	//check if data exists in the table
+	if (mysqli_num_rows($result)>0) {
+		// fetch the data from rows
+		echo "<table border='1'>";
+		$col = mysqli_fetch_fields($result);
+		//print the colums
+		echo "<tr>";
+		foreach ($col as $value) {
+			//return an object
+			//print_r($value);
+			echo "<td>$value->name</td>";
+		}
+		echo "</tr>";
+		
+		while ($row = mysqli_fetch_assoc($result)) {
+			//print the data in a table format
+			echo "<tr>";
+			foreach ($row as $key => $value) {
+				echo "<td>$value</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table>";
+	} else {
+		echo "No results";
+	}
+	
+} catch (Exception $e) {
+	die($e->getMessage());
+}
+}
+
+
+//PrintTable("books",$connect);
+<?php
+//get the db connection file
+require_once 'dbconf.php';
+
+function PrintTable($tableName,$connect)
+{
+
+try {
+
+	//Query
+	$sql = "SELECT * FROM $tableName";
+
+	//excute the quey
+	$result = mysqli_query($connect,$sql);
+	//check if data exists in the table
+	if (mysqli_num_rows($result)>0) {
+		// fetch the data from rows
+		echo "<table border='1'>";
+		$col = mysqli_fetch_fields($result);
+		//print the colums
+		echo "<tr>";
+		foreach ($col as $value) {
+			//return an object
+			//print_r($value);
+			echo "<td>$value->name</td>";
+		}
+		echo "</tr>";
+		
+		while ($row = mysqli_fetch_assoc($result)) {
+			//print the data in a table format
+			echo "<tr>";
+			foreach ($row as $key => $value) {
+				echo "<td>$value</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table>";
+	} else {
+		echo "No results";
+	}
+	
+} catch (Exception $e) {
+	die($e->getMessage());
+}
+}
+
+
+PrintTable("books",$connect);
